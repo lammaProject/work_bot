eeeb758773e26f9fb09e497f154aeef7d931d459
anton <livegamelife@gmail.com>
2024-10-06 00:54:11 +0500

    wip

--- bot.py
+++ bot.py
@@ -3,8 +3,14 @@ import logging
 import os
 
 from dotenv import load_dotenv
-from telegram import Update
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    CallbackQueryHandler,
+    filters,
+    MessageHandler,
+)
 
 from my_calendar.calendar import calendar_command, calendar_callback
 from my_calendar.notification import notification_loop
@@ -24,10 +30,17 @@ async def error_handler(update: Update, context):
     logger.error(f"Exception while handling an update: {context.error}")
 
 
+async def start(update: Update, context):
+    keyboard = [["Календарь"]]
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    await update.message.reply_text("Скоро на завод", reply_markup=reply_markup)
+
+
 async def main():
     app = Application.builder().token(TOKEN).build()
 
-    app.add_handler(CommandHandler("calendar", calendar_command))
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.Text(["Календарь"]), calendar_command))
     app.add_handler(CallbackQueryHandler(calendar_callback))
     app.add_error_handler(error_handler)
 
